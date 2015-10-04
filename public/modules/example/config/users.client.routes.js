1207072aeb7b8927e3d213b98532cf02470cf201
salismt <tsalis.mohammad@gmail.com>
2015-10-04 15:34:58 +0700

    finished project with angular bug / front-end bug

--- public/modules/example/config/users.client.routes.js
+++ public/modules/example/config/users.client.routes.js
@@ -1,7 +1,7 @@
 angular.module('example').config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
 		.when('/', {
-			templateUrl: 'modules/example/views/example.client.view.html'
+			templateUrl: 'example/views/example.client.view.html'
 		})
 		.otherwise({
 			redirectTo: '/'
